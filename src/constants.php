4cdcce64a7b0f96a796fb0f7375c1d43b335e0bb
itosho <sho.ito@supership.jp>
2017-10-21 04:54:49 +0900

    add dictionary class

---
+++ src/constants.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Itosho\IdolEcConstantsPhp\Constant;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Yosymfony\Toml\Toml;
+
+class Dictionary
+{
+    private $constants;
+
+    public function __construct()
+    {
+        $this->constants = Toml::Parse(__DIR__ . '/../modules/constants/constants.toml');
+    }
+
+    public function getAll()
+    {
+        return $this->constants;
+    }
+
+    public function get($keyName)
+    {
+        return $this->constants[$keyName];
+    }
+}
